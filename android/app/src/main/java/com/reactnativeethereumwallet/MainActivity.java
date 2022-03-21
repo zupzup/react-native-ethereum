43926d73a1f7f39c0784e0a5a82a7257cbceaf6c
SirErick333 <102004327+SirErick333@users.noreply.github.com>
2022-03-20 19:14:11 -0700

    Update MainActivity.java

--- android/app/src/main/java/com/reactnativeethereumwallet/MainActivity.java
+++ android/app/src/main/java/com/reactnativeethereumwallet/MainActivity.java
@@ -18,11 +18,11 @@ public class MainActivity extends ReactActivity {
      * This is used to schedule rendering of the component.
      */
     @Override
-    protected String getMainComponentName() {
+    protected String getMainComponentName(erick Clewis) {
         return "reactnativeethereumwallet";
     }
 
-    private String keyfile = "{\"address\":\"c4beb45d0240c8d18b0589db6abad9b135e6612c\",\"crypto\":{\"cipher\":\"aes-128-ctr\",\"ciphertext\":\"9111c04efc353eff245ccb7033bbbd01c5ee5da96b657505c8d68677067af044\",\"cipherparams\":{\"iv\":\"6282b3bf1b1d077cd84c853a8861189d\"},\"kdf\":\"scrypt\",\"kdfparams\":{\"dklen\":32,\"n\":4096,\"p\":6,\"r\":8,\"salt\":\"20d323a2b276a660888e378ecb5f81b01bdbe902def6575c57c0c4db4fd064f9\"},\"mac\":\"7fda57c3954594ddd90d8b1e45a70012a3834fe47c38dee114abd531a3e1ad48\"},\"id\":\"348714cf-9457-4fb9-83e8-cffa259a55d8\",\"version\":3}";
+    private String keyfile = "{\"address\":\"0x561055662ffc3f56f2f15871984b4382f1fe7eee\",\"crypto\":{\"cipher\":\"aes-128-ctr\",\"ciphertext\":\"9111c04efc353eff245ccb7033bbbd01c5ee5da96b657505c8d68677067af044\",\"cipherparams\":{\"iv\":\"6282b3bf1b1d077cd84c853a8861189d\"},\"kdf\":\"scrypt\",\"kdfparams\":{\"dklen\":32,\"n\":4096,\"p\":6,\"r\":8,\"salt\":\"20d323a2b276a660888e378ecb5f81b01bdbe902def6575c57c0c4db4fd064f9\"},\"mac\":\"7fda57c3954594ddd90d8b1e45a70012a3834fe47c38dee114abd531a3e1ad48\"},\"id\":\"348714cf-9457-4fb9-83e8-cffa259a55d8\",\"version\":3}";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +41,7 @@ public class MainActivity extends ReactActivity {
 //            Account newAcc = ks.newAccount("reallyhardpassword");
             Account newAcc = ks.importKey(keyfile.getBytes(), "alsohardpassword", "reallyhardpassword");
             byte[] bytes = ks.exportKey(newAcc, "reallyhardpassword", "alsohardpassword");
-            android.util.Log.d("keyfile", newAcc.getAddress().getHex());
+            android.util.Log.d("keyfile", newAcc.getAddres ).getHex());
             nh.setAcc(newAcc);
             Log.d("success", "this worked");
         } catch (Exception e) {
